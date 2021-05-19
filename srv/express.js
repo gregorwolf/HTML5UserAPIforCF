936276f0e926d761f5f38671ef61f86c6d9ab3ab
Gregor Wolf <gregor.wolf@gmail.com>
2021-05-19 23:59:41 +0200

    add ping via SAP Cloud SDK

--- srv/express.js
+++ srv/express.js
@@ -1,42 +1,44 @@
 // load modules
-const express = require('express')
+const express = require("express");
 // For authentication test
-const passport = require('passport')
-const xsenv = require('@sap/xsenv')
+const passport = require("passport");
+const xsenv = require("@sap/xsenv");
 xsenv.loadEnv();
-const JWTStrategy = require('@sap/xssec').JWTStrategy
-const services = xsenv.getServices({ xsuaa: { tags: 'xsuaa' }})
-const jwtDecode = require('jwt-decode')
-passport.use(new JWTStrategy(services.xsuaa))
+const JWTStrategy = require("@sap/xssec").JWTStrategy;
+const services = xsenv.getServices({ xsuaa: { tags: "xsuaa" } });
+const jwtDecode = require("jwt-decode");
+passport.use(new JWTStrategy(services.xsuaa));
+const { executeHttpRequest } = require("@sap-cloud-sdk/core");
 
 // config
-const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 4004
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 4004;
+const destination = {
+  destinationName: process.env.DESTINATION || "SAP_ABAP_BACKEND",
+};
 
 function getJWT(req) {
   const jwt = /^Bearer (.*)$/.exec(req.headers.authorization)[1];
   return jwt;
 }
 
-;(async () => {
+(async () => {
   // create new app
-  const app = express()
+  const app = express();
   // Authentication using JWT
-  await app.use(passport.initialize())
-  await app.use(
-    passport.authenticate('JWT', { session: false })
-  )
+  await app.use(passport.initialize());
+  await app.use(passport.authenticate("JWT", { session: false }));
 
-  await app.get('/api/userInfo', function (req, res) {
+  await app.get("/api/userInfo", function (req, res) {
     res.header("Content-Type", "application/json");
-    res.send(JSON.stringify(req.user))
-  })
+    res.send(JSON.stringify(req.user));
+  });
 
-  await app.get('/api/jwt', function (req, res) {
+  await app.get("/api/jwt", function (req, res) {
     res.header("Content-Type", "application/json");
-    res.send(JSON.stringify({ "JWT": getJWT(req) }))
-  })
-  await app.get('/api/jwtdecode', function (req, res) {
+    res.send(JSON.stringify({ JWT: getJWT(req) }));
+  });
+  await app.get("/api/jwtdecode", function (req, res) {
     if (!req.user) {
       res.statusCode = 403;
       res.end(`Missing JWT Token`);
@@ -45,13 +47,47 @@ function getJWT(req) {
       res.header("Content-Type", "application/json");
       res.end(`${JSON.stringify(jwtDecode(getJWT(req)))}`);
     }
-  })
+  });
+
+  await app.get("/api/public/ping", async function (req, res) {
+    try {
+      const resultServiceCollection = await executeHttpRequest(destination, {
+        method: "get",
+        url: "/sap/public/ping",
+      });
+      res.send(resultServiceCollection.data);
+    } catch (error) {
+      let message = error.message;
+      if (error.rootCause && error.rootCause.message) {
+        message += " Root Cause: " + error.rootCause.message;
+      }
+      res.send(message);
+    }
+  });
+
+  await app.get("/api/bc/ping", async function (req, res) {
+    try {
+      const resultServiceCollection = await executeHttpRequest(destination, {
+        method: "get",
+        url: "/sap/bc/ping",
+      });
+      res.send(resultServiceCollection.data);
+    } catch (error) {
+      let message = error.message;
+      if (error.rootCause && error.rootCause.message) {
+        message += " Root Cause: " + error.rootCause.message;
+      }
+      res.send(message);
+    }
+  });
 
   // start server
   const server = app.listen(port, host, () => {
-    console.info(`app is listing at ${port}`)
+    console.info(`app is listing at ${port}`);
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
-  })
-  server.on('error', error => console.error(error.stack))
-})()
+    console.log(
+      `The script uses approximately ${Math.round(used * 100) / 100} MB`
+    );
+  });
+  server.on("error", (error) => console.error(error.stack));
+})();
