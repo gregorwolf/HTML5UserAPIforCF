faefc9153d8034b5a65f7cca285897ff85bec07c
Gregor Wolf <gregor.wolf@gmail.com>
2021-11-08 23:13:39 +0100

    load version from manifest.json

--- HTML5Module/webapp/controller/Home.controller.js
+++ HTML5Module/webapp/controller/Home.controller.js
@@ -1,10 +1,21 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/Manifest"],
+  function (Controller, Manifest) {
+    "use strict";
 
-  return Controller.extend(
-    "com.sap.sapmentors.html5userapiforcf-ui.controller.Home",
-    {
-      onInit: function () {},
-    }
-  );
-});
+    return Controller.extend(
+      "com.sap.sapmentors.html5userapiforcf-ui.controller.Home",
+      {
+        onInit: function () {
+          let oManifest = Manifest.load({ manifestUrl: "./manifest.json" });
+          let version = oManifest.getEntry(
+            "/sap.app/applicationVersion/version"
+          );
+          console.log(version);
+          let versionText = this.byId("versionText");
+          versionText.setText(version);
+        },
+      }
+    );
+  }
+);
