9d49b3fc1a646980013de6b357a90d8a01bf5c02
Gregor Wolf <gregor.wolf@gmail.com>
2025-12-02 21:21:42 +0100

    add middleware that sets sap-client header based on scope

--- approuter/my-ext.js
+++ approuter/my-ext.js
@@ -8,6 +8,27 @@ module.exports = {
     ],
     beforeRequestHandler: [
       {
+        path: "/sap",
+        handler: function sapClientMiddleware(req, res, next) {
+          if (req.user && req.user.scopes && Array.isArray(req.user.scopes)) {
+            const scopePrefix = "HTML5UserAPIforCF!t5585.Client";
+            const matchingScope = req.user.scopes.find(
+              (scope) =>
+                typeof scope === "string" && scope.startsWith(scopePrefix)
+            );
+
+            if (matchingScope) {
+              const clientNumber = matchingScope.substring(scopePrefix.length);
+              if (clientNumber) {
+                req.headers["sap-client"] = clientNumber;
+                console.log("Set sap-client header to: %s", clientNumber);
+              }
+            }
+          }
+          return next();
+        },
+      },
+      {
         path: "/services/userapi/currentUser",
         handler: function myMiddleware(req, res, next) {
           if (!req.user) {
